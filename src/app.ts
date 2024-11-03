871b4ce74679d402e842122841626795d9950b91
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-04 00:23:39 +0600

    create-routes-contrllar-service-file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import  express, { Application, Request, Response }  from "express";
 import cors from "cors";
+import { UserRoutes } from "./app/modules/User/user.routes";
+
 
 const app: Application = express();
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) =>{
     res.send("PH Helth Care Server....")
-})
+});
+
+app.use('/api/v1/user', UserRoutes);
 
 export default app;
