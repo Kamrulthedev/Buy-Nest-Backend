c076fc0cac7e9f804e2124f25a2df5113d0238eb
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-04 08:45:20 +0600

    use-service data-commit

--- src/app/modules/User/user.controllar.ts
+++ src/app/modules/User/user.controllar.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { UserServices } from "./user.service";
 
 const CreateAdminSQ = async (req: Request, res: Response) => {
-    console.log(req.body)
-  const result = await UserServices.CreateAdmin();
+  const data = req.body;
+  const result = await UserServices.CreateAdmin(data);
   res.send(result);
 };
 
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,6 +1,11 @@
 
 
-const CreateAdmin = async() => {
+const CreateAdmin = async(data : any) => {
+    console.log("this is service console",data);
+
+
+    
+
     return {
         message : "This is Create Admin Message"
     }
