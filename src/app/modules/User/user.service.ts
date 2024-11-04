d67536f366eb9ad8ed0959ff70d24de74100ca15
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-04 09:04:24 +0600

    create-user-successfully-commit

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,17 +1,29 @@
+import { UserRole } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-const CreateAdmin = async(data : any) => {
-    console.log("this is service console",data);
+const CreateAdmin = async (data: any) => {
+  const userData = {
+    password: data.password,
+    email: data.admin.email,
+    role: UserRole.ADMIN,
+  };
 
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const createdUserData = await transactionClient.user.create({
+      data: userData,
+    });
 
-    
+    const createdAdmindata = await transactionClient.admin.create({
+      data: data.admin,
+    });
+    return createdAdmindata;
+  });
 
-    return {
-        message : "This is Create Admin Message"
-    }
+  return result;
 };
 
-
 export const UserServices = {
-    CreateAdmin
+  CreateAdmin,
 };
