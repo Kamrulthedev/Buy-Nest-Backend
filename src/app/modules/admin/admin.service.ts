6eb2e9c297aae0389cf170d507932c4e1fadc905
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-04 11:02:16 +0600

    advance-search-system-commit

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,28 +3,35 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const GetAdmins = async (params: any) => {
-  const andCondions : Prisma.AdminWhereInput[] = [];
+  const andCondions: Prisma.AdminWhereInput[] = [];
+
+  //   [
+  //     {
+  //       name: {
+  //         contains: params.searchTram,
+  //         mode: "insensitive",
+  //       },
+  //     },
+  //     {
+  //       email: {
+  //         contains: params.searchTram,
+  //         mode: "insensitive",
+  //       },
+  //     },
+  //   ],
 
   if (params.searchTram) {
     andCondions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTram,
-            mode: "insensitive",
-          },
+      OR: ["name", "email"].map((field) => ({
+        [field]: {
+          contains: params.searchTram,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTram,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
 
-  const whereCondition : Prisma.AdminWhereInput = { AND: andCondions };
+  const whereCondition: Prisma.AdminWhereInput = { AND: andCondions };
   const result = await prisma.admin.findMany({
     where: whereCondition,
   });
