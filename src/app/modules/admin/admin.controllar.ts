050878ce8d98721170ddc32c4b0a911203f4ceea
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-07 02:08:29 +0600

    send-Response-fumction-create-commit

--- src/app/modules/admin/admin.controllar.ts
+++ src/app/modules/admin/admin.controllar.ts
@@ -3,16 +3,38 @@ import { AdminServices } from "./admin.service";
 import { pick } from "../../../shared/pick";
 import { AdminFilterableFields } from "./admin.constent";
 
+
+const sendResponse = <T>(res : Response, jsonData : {
+  statusCode : number,
+  success : boolean,
+  message : string,
+  meta? : {
+    page : number,
+    limit : number,
+    total : number
+  },
+  data : T | null | undefined
+}) =>{
+  res.status(jsonData.statusCode).json({
+    success : jsonData.success,
+    message : jsonData.message,
+    meta : jsonData.meta || null || undefined,
+    data : jsonData.data || null || undefined
+  })
+}
+
+
 const GetAdminsDB = async (req: Request, res: Response) => {
   try {
     const filter = pick(req.query, AdminFilterableFields);
     const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
     const result = await AdminServices.GetAdmins(filter, options);
-    res.status(200).json({
-      success: true,
-      message: "Admins Fatched!",
-      meta: result.meta,
-      data: result.data,
+    sendResponse(res, {
+      statusCode : 200,
+      success : true,
+      message : "Admin Data fetched Successfully!",
+      meta : result.meta,
+      data : result.data
     });
   } catch (err: unknown) {
     const error = err as Error;
@@ -28,11 +50,13 @@ const GetByIdDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminServices.GetById(id);
-    res.status(200).json({
-      success: true,
-      message: "Admins Fatched!",
-      data: result,
-    });
+      sendResponse(res, {
+        statusCode : 200,
+        success: true,
+        message: "Admin Data Fatched Successfully!",
+        data: result,
+      });
+
   } catch (err: unknown) {
     const error = err as Error;
     res.status(500).json({
@@ -47,11 +71,12 @@ const UpdateAdminDB = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await AdminServices.UpdateAdmin(id, req.body);
-    res.status(200).json({
-      success: true,
-      message: "Admin Data Updaetd!",
-      data: result,
-    });
+   sendResponse(res, {
+    statusCode : 200,
+    success: true,
+    message: "Admin Data Updaetd!",
+    data: result,
+   });
   } catch (err: unknown) {
     const error = err as Error;
     res.status(500).json({
@@ -67,11 +92,12 @@ const DeleteFromAdminDB = async(req: Request, res: Response) =>{
   const { id } = req.params;
   try {
     const result = await AdminServices.DeleteFromAdmin(id);
-    res.status(200).json({
-      success: true,
-      message: "Admin Data Deleted!",
-      data: result,
-    });
+  sendResponse (res, {
+    statusCode : 200,
+    success: true,
+    message: "Admin Data Deleted!",
+    data: result,
+  });
   } catch (err: unknown) {
     const error = err as Error;
     res.status(500).json({
@@ -87,11 +113,12 @@ const SoftDeleteFromAdminDB = async(req: Request, res: Response) =>{
   const { id } = req.params;
   try {
     const result = await AdminServices.SoftDeleteFromAdmin(id);
-    res.status(200).json({
-      success: true,
-      message: "Admin Data Deleted Successfully!",
-      data: result,
-    });
+  sendResponse(res, {
+    statusCode : 200,
+    success: true,
+    message: "Admin Data Deleted Successfully!",
+    data: result,
+  })
   } catch (err: unknown) {
     const error = err as Error;
     res.status(500).json({
