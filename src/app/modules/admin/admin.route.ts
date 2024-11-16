7a425d8b173773aad071901c2bb45dca39c4448a
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-16 16:32:18 +0600

    validate-with-zod-commit

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,23 +1,34 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AdminControllars } from "./admin.controllar";
+import { AnyZodObject, z } from "zod";
 
 const router = express.Router();
 
-export const validateRequest = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log("data", req.body);
-  console.log("Checker Middleware.....");
-  next();
+const updated = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+export const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body : req.body
+      });
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
 };
 
 router.get("/admins", AdminControllars.GetAdminsDB);
 
 router.get("/:id", AdminControllars.GetByIdDB);
 
-router.patch("/:id", validateRequest, AdminControllars.UpdateAdminDB);
+router.patch("/:id", validateRequest(updated), AdminControllars.UpdateAdminDB);
 
 router.delete("/:id", AdminControllars.DeleteFromAdminDB);
 
