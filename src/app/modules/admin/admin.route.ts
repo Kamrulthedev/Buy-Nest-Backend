0f9c1523634bcc0f39c0fccd9812a7c9246aa010
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-16 15:06:53 +0600

    update-middleware-commit

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,14 +1,23 @@
-import express  from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminControllars } from "./admin.controllar";
 
-
 const router = express.Router();
+
+export const validateRequest = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("data", req.body);
+  console.log("Checker Middleware.....");
+  next();
+};
 
 router.get("/admins", AdminControllars.GetAdminsDB);
 
 router.get("/:id", AdminControllars.GetByIdDB);
 
-router.patch("/:id", AdminControllars.UpdateAdminDB);
+router.patch("/:id", validateRequest, AdminControllars.UpdateAdminDB);
 
 router.delete("/:id", AdminControllars.DeleteFromAdminDB);
 
