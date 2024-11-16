5d660181470753a30ba7c1e77b85abd4a66bd541
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-16 13:37:18 +0600

    create-a-catchasync-function-commit

--- src/app/modules/admin/admin.controllar.ts
+++ src/app/modules/admin/admin.controllar.ts
@@ -1,101 +1,68 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { AdminServices } from "./admin.service";
 import { pick } from "../../../shared/pick";
 import { AdminFilterableFields } from "./admin.constent";
 import sendResponse from "../../../shared/sendResponse";
+import { catchAsync } from "../../../shared/catchAsync";
 
-const GetAdminsDB = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const filter = pick(req.query, AdminFilterableFields);
-    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
-    const result = await AdminServices.GetAdmins(filter, options);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin Data fetched Successfully!",
-      meta: result.meta,
-      data: result.data,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
 
-const GetByIdDB = async (req: Request, res: Response) => {
+const GetAdminsDB = catchAsync(async (req, res) => {
+  const filter = pick(req.query, AdminFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await AdminServices.GetAdmins(filter, options);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin Data fetched Successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const GetByIdDB = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.GetById(id);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin Data Fatched Successfully!",
-      data: result,
-    });
-  } catch (err: unknown) {
-    const error = err as Error;
-    res.status(500).json({
-      success: false,
-      message: error?.name || "Something Went Wrong",
-      err: error,
-    });
-  }
-};
+  const result = await AdminServices.GetById(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin Data Fatched Successfully!",
+    data: result,
+  });
+});
 
-const UpdateAdminDB = async (req: Request, res: Response, next : NextFunction) => {
+const UpdateAdminDB = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.UpdateAdmin(id, req.body);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin Data Updaetd!",
-      data: result,
-    });
-  } catch (err) {
- next(err)
-  }
-};
+  const result = await AdminServices.UpdateAdmin(id, req.body);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin Data Updaetd!",
+    data: result,
+  });
+});
 
-const DeleteFromAdminDB = async (req: Request, res: Response) => {
+const DeleteFromAdminDB = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.DeleteFromAdmin(id);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin Data Deleted!",
-      data: result,
-    });
-  } catch (err: unknown) {
-    const error = err as Error;
-    res.status(500).json({
-      success: false,
-      message: error?.name || "Somting Went Wrong!",
-      err: error,
-    });
-  }
-};
+  const result = await AdminServices.DeleteFromAdmin(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin Data Deleted!",
+    data: result,
+  });
+});
 
 //soft Delete
-const SoftDeleteFromAdminDB = async (req: Request, res: Response) => {
+const SoftDeleteFromAdminDB = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    const result = await AdminServices.SoftDeleteFromAdmin(id);
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin Data Deleted Successfully!",
-      data: result,
-    });
-  } catch (err: unknown) {
-    const error = err as Error;
-    res.status(500).json({
-      success: false,
-      message: error?.name || "Somting Went Wrong!",
-      err: error,
-    });
-  }
-};
+  const result = await AdminServices.SoftDeleteFromAdmin(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin Data Deleted Successfully!",
+    data: result,
+  });
+});
 
 export const AdminControllars = {
   GetAdminsDB,
