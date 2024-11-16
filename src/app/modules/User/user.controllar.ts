1c4b6718536aedf3ab1fad3da93aa6406282702f
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-16 13:38:46 +0600

    use-catchAsync-fucntion-commit

--- src/app/modules/User/user.controllar.ts
+++ src/app/modules/User/user.controllar.ts
@@ -1,25 +1,16 @@
 import { Request, Response } from "express";
 import { UserServices } from "./user.service";
+import { catchAsync } from "../../../shared/catchAsync";
 
-const CreateAdminSQ = async (req: Request, res: Response) => {
+const CreateAdminSQ = catchAsync(async (req, res) => {
   const data = req.body;
-  try {
     const result = await UserServices.CreateAdmin(data);
     res.status(200).json({
       success: true,
       message: "Admin Created Successfully!",
       data: result,
     });
-  } catch (err: unknown) {
-    const error = err as Error;
-    res.status(500).json({
-      success: false,
-      message: error?.name || "Something Went Wrong",
-      err: error,
-    });
-  }
-  
-};
+});
 
 export const UserControllars = {
   CreateAdminSQ,
