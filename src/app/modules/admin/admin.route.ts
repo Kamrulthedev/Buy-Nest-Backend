28caedf7a1c1a2f5ec4757698fe52e458c7e3d59
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-16 16:37:17 +0600

    create-middlewere-file-commit

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,34 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AdminControllars } from "./admin.controllar";
 import { AnyZodObject, z } from "zod";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { ValidationWithZod } from "./admin.validation";
 
 const router = express.Router();
-
-const updated = z.object({
-  body: z.object({
-    name: z.string().optional(),
-    contactNumber: z.string().optional(),
-  }),
-});
-
-export const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body : req.body
-      });
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 router.get("/admins", AdminControllars.GetAdminsDB);
 
 router.get("/:id", AdminControllars.GetByIdDB);
 
-router.patch("/:id", validateRequest(updated), AdminControllars.UpdateAdminDB);
+router.patch("/:id", validateRequest(ValidationWithZod.UpdateValidation), AdminControllars.UpdateAdminDB);
 
 router.delete("/:id", AdminControllars.DeleteFromAdminDB);
 
--- src/app/modules/admin/admin.validation.ts
+++ src/app/modules/admin/admin.validation.ts
@@ -1,1 +1,13 @@
-import {z} from "zod";
+import { z } from "zod";
+
+const UpdateValidation = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+
+export const ValidationWithZod = {
+    UpdateValidation
+};
