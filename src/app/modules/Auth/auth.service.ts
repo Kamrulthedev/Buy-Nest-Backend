040ce8a59c3ee172c2493aa5187611c98dd7a268
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-17 08:24:54 +0600

    update-commit

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../../shared/SharedPrisma";
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const loginUser = async (payload: { email: string; password: string }) => {
   const userData = await prisma.user.findUniqueOrThrow({
@@ -8,13 +9,28 @@ const loginUser = async (payload: { email: string; password: string }) => {
     },
   });
 
-  const isCorrentPassword : boolean = await bcrypt.compare(payload.password, userData.password);
+  const isCorrentPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+      status: userData.status,
+    },
+    "123456789",
+    {
+      algorithm : "HS256",
+      expiresIn: '15m'
+    }
+  );
+
+  console.log({accessToken})
 
   return userData;
 };
-
-
-
 
 export const AuthService = {
   loginUser,
