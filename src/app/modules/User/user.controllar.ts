05415de604944bcc2975b94ca9335733c8a1b901
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-25 07:23:46 +0600

    update-admin-service-code

--- src/app/modules/User/user.controllar.ts
+++ src/app/modules/User/user.controllar.ts
@@ -3,8 +3,7 @@ import { UserServices } from "./user.service";
 import { catchAsync } from "../../../shared/catchAsync";
 
 const CreateAdminSQ = catchAsync(async (req, res) => {
-  const data = req.body;
-    const result = await UserServices.CreateAdmin(data);
+    const result = await UserServices.CreateAdmin(req);
     res.status(200).json({
       success: true,
       message: "Admin Created Successfully!",
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -3,27 +3,30 @@ import * as bcrypt from "bcrypt";
 import { prisma } from "../../../shared/SharedPrisma";
 
 
-const CreateAdmin = async (data: any) => {
-  const hashedPassword: string = await bcrypt.hash(data.password, 12);
+const CreateAdmin = async (req: any) => {
+  console.log("File: ", req.file);
+  console.log("Data: ", req.body.data);
+  
+  // const hashedPassword: string = await bcrypt.hash(data.password, 12);
 
-  const userData = {
-    password: hashedPassword,
-    email: data.admin.email,
-    role: UserRole.ADMIN,
-  };
+  // const userData = {
+  //   password: hashedPassword,
+  //   email: data.admin.email,
+  //   role: UserRole.ADMIN,
+  // };
 
-  const result = await prisma.$transaction(async (transactionClient) => {
-    await transactionClient.user.create({
-      data: userData,
-    });
+  // const result = await prisma.$transaction(async (transactionClient) => {
+  //   await transactionClient.user.create({
+  //     data: userData,
+  //   });
 
-    const createdAdmindata = await transactionClient.admin.create({
-      data: data.admin,
-    });
-    return createdAdmindata;
-  });
+  //   const createdAdmindata = await transactionClient.admin.create({
+  //     data: data.admin,
+  //   });
+  //   return createdAdmindata;
+  // });
 
-  return result;
+  // return result;
 };
 
 export const UserServices = {
