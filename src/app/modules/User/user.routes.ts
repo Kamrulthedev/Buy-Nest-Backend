d2f5289492a698b432c909278cc139c2c4bce7f1
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-25 06:52:14 +0600

    useing-multer-and-file-uploads-commit

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { UserControllars } from "./user.controllar";
 import { auth } from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import upload from "../../../helpars/fileUploads";
 
 const router = express.Router();
 
 
-router.post("/create-admin", auth("ADMIN"), UserControllars.CreateAdminSQ);
+router.post("/create-admin", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    upload.single('file'),
+    UserControllars.CreateAdminSQ);
 
 export const UserRoutes = router;
