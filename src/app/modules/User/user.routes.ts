69e0a2fbfc324016ba708e967b4c23e60c66f1a4
Kamrul Hassan <kamrulthedev@gmail.com>
2024-11-25 07:13:33 +0600

    setup-cloudinary-commit

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,13 +2,13 @@ import express from "express";
 import { UserControllars } from "./user.controllar";
 import { auth } from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
-import upload from "../../../helpars/fileUploads";
+import { Fileuploader } from "../../../helpars/fileUploads";
 
 const router = express.Router();
 
 
 router.post("/create-admin", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-    upload.single('file'),
+    Fileuploader.upload.single('file'),
     UserControllars.CreateAdminSQ);
 
 export const UserRoutes = router;
--- src/helpars/fileUploads.ts
+++ src/helpars/fileUploads.ts
@@ -1,5 +1,15 @@
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from 'cloudinary';
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dak98cpxr', 
+        api_key: '862687512999954', 
+        api_secret: 'e_p7JvI_OlNh7d1qqg9V2WhGhcU' 
+    });
+    
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -12,4 +22,43 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 
-export default upload;
+const uploadToCloudinary = async(file : any) =>{
+    (async function() {
+        // Upload an image
+         const uploadResult = await cloudinary.uploader
+           .upload(
+               'E:\Next\L-2-W.D.C-PH\Misson-6-MYSQL\PH HealthCare\PH HealthCare-Server/uploads/my.jpg', {
+                   public_id: 'shoes',
+               }
+           )
+           .catch((error) => {
+               console.log(error);
+           });
+        
+        console.log(uploadResult);
+        
+        // Optimize delivery by resizing and applying auto-format and auto-quality
+        const optimizeUrl = cloudinary.url('shoes', {
+            fetch_format: 'auto',
+            quality: 'auto'
+        });
+        
+        console.log(optimizeUrl);
+        
+        // Transform the image: auto-crop to square aspect_ratio
+        const autoCropUrl = cloudinary.url('shoes', {
+            crop: 'auto',
+            gravity: 'auto',
+            width: 500,
+            height: 500,
+        });
+        
+        console.log(autoCropUrl);    
+    })();
+};
+
+
+export const Fileuploader = {
+  upload,
+  uploadToCloudinary
+};
