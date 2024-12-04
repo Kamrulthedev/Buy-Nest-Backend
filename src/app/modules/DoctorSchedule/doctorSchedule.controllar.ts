7eec31b546702ab1bd98d2d29536861f101a5c5c
Kamrul Hassan <kamrulthedev@gmail.com>
2024-12-04 13:32:38 +0600

    update-schedule-commit

--- src/app/modules/DoctorSchedule/doctorSchedule.controllar.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.controllar.ts
@@ -1,9 +1,11 @@
 import { catchAsync } from "../../../shared/catchAsync";
+import { pick } from "../../../shared/pick";
 import sendResponse from "../../../shared/sendResponse";
+import { scheduleFilterableFields } from "./doctorSchedule.constent";
 import { DoctorScheduleServices } from "./doctorSchedule.service";
 
 
-const InsertIntoDB = catchAsync(async (req , res) => {
+const InsertIntoDB = catchAsync(async (req, res) => {
     const user = req.user;
     const result = await DoctorScheduleServices.InsertInto(user, req.body);
     sendResponse(res, {
@@ -16,6 +18,22 @@ const InsertIntoDB = catchAsync(async (req , res) => {
 
 
 
+const GetAllFromDB = catchAsync(async (req, res) => {
+    const filters = pick(req.query, scheduleFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await DoctorScheduleServices.GetAllFrom(filters, options);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Doctor Schedule retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+
+
 export const DoctorScheduleControllers = {
-    InsertIntoDB
+    InsertIntoDB,
+    GetAllFromDB
 };
--- src/app/modules/DoctorSchedule/doctorSchedule.route.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.route.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get(
     '/',
     auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
-    // DoctorScheduleController.getAllFromDB
+    DoctorScheduleControllers.GetAllFromDB
 );
 
 router.get(
